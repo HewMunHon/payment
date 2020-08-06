dbf16d20968933e7160b949626fa966744f2e2e1
Hew Mun Hon <hewmh-wm19@student.tarc.edu.my>
2020-08-06 11:00:40 +0800

    commit by mun hon

---
+++ app/src/main/java/c/example/payment/MainActivity.java
@@ -0,0 +1,35 @@
+package c.example.payment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.spinner);
+
+
+        // Create an ArrayAdapter using the string array and default spinner
+        // layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.labels_array,
+                android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner.
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+    }
+}
